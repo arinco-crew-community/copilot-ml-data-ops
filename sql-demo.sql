8f96591f0fc925b08d9beecd50c010b063008482
Nick Marie <56579992+nickcode95@users.noreply.github.com>
2024-06-03 16:01:41 +1000

    Update docs with Copilot prompt that includes Language:SQL to prevent copilot from generating MYSQL code instead of SQL

--- sql-demo.sql
+++ sql-demo.sql
@@ -1,5 +1,6 @@
 -- when I ran it, just having the 4 drop tables as a baseline, then when I typed "create", copilot was creating all the tables.  
 -- all subsequent code was generated from comments
+-- Including the Language: sql comment prevents Copilot from generating MYSQL code instead of SQL code
 -- for data generation, it can give weird code if "using loops" is not added
 
 
@@ -10,9 +11,13 @@ drop table if exists dbo.users
 
 
 -- show all tables in the database
+-- Language: sql
 
 -- insert 100 random data into all existing tables in the database (users, posts, comments and likes) using loops
+-- Language: sql
 
 -- show all posts order by likes count 
+-- Language: sql
 
 -- show all posts order by likes count using ctes
+-- Language: sql
