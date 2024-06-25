7a32ec8b479ab37a3c429889d558722ea524b87b
tw3lveparsecs <aj.bajada@outlook.com>
2024-06-25 13:55:13 +1000

    updated data ops workshop and format

--- readme.md
+++ readme.md
@@ -1,18 +1,27 @@
-# Copilot ML / Data Ops
+# GitHub Copilot Machine Learning & Data Ops Workshop
 
-## DataOps
+In this fun workshop, you will learn how to use GitHub Copilot to help you write code for DataOps and Machine Learning tasks.
 
-[sql-demo.sql](sql-demo.sql) demonstrates that you can use copilot to help you write SQL. Example outputs are in [sql-demo-example-output.sql](sql-demo-example-output.sql).
+Estimated time to complete: `10 to 30 minutes`
 
-Prerequisites:
-- [Azure Data Studio](https://learn.microsoft.com/en-us/azure-data-studio/download-azure-data-studio?view=sql-server-ver16&tabs=win-install%2Cwin-user-install%2Credhat-install%2Cwindows-uninstall%2Credhat-uninstall)
-- [GitHub Copilot extension for Azure Data Studio](https://learn.microsoft.com/en-us/azure-data-studio/extensions/github-copilot-extension-overview#install-the-github-copilot-extension)
+## Instructions
+
+Inside the `.instructions` folder, you will find a number of markdown files that will guide you through the workshop.
+
+| File                                                                             | Description                                              |
+| -------------------------------------------------------------------------------- | -------------------------------------------------------- |
+| [1.setup.md](./.instructions/1.setup.md)                                         | Instructions on how to set up your environment.          |
+| [2.dataops-exercises.md](./.instructions/2.dataops-exercises.md)                 | DataOps exercises for participants to complete.          |
+| [3.machine-learning-exercises.md](.instructions/3.machine-learning-exercises.md) | Machine Learning exercises for participants to complete. |
+
+> Note: below is work in progress and will be updated soon.
 
 ## Machine Learning
 
 [ml-penguin-demo.ipynb](ml-penguin-demo.ipynb) demonstrates that you can use copilot to help you write code for machine learning. Instructions for how to run this are in [ml-penguin-demo-example-output.ipynb](ml-penguin-demo-example-output.ipynb).
 
 Prerequisites:
+
 - `python.exe -m pip install --upgrade pip --user`
 - `pip install pandas numpy matplotlib seaborn scikit-learn --user`
 
--- sql-demo-example-output.sql
+++ sql-demo-example-output.sql
@@ -1,123 +1,77 @@
--- when I ran it, just having the 4 drop tables as a baseline, then when I typed "create", copilot was creating all the tables.  
--- all subsequent code was generated from comments
--- for data generation, it can give weird code if "using loops" is not added
-
+-- Description: This file contains the output of the SQL demo example file
 
 drop table if exists dbo.comments
 drop table if exists dbo.likes
 drop table if exists dbo.posts
 drop table if exists dbo.users
 
+-- create tables with foreign and primary keys
 create table dbo.users
 (
-    id int identity(1,1) not null,
-    name varchar(50) not null,
-    primary key (id)
-);
-
+    id int primary key,
+    name varchar(255)
+)
 create table dbo.posts
 (
-    id int identity(1,1) not null,
-    title varchar(50) not null,
-    content varchar(50) not null,
-    user_id int not null,
-    primary key (id),
+    id int primary key,
+    user_id int,
+    title varchar(255),
+    content text,
     foreign key (user_id) references dbo.users(id)
-);
-
+)
 create table dbo.comments
 (
-    id int identity(1,1) not null,
-    content varchar(50) not null,
-    user_id int not null,
-    post_id int not null,
-    primary key (id),
+    id int primary key,
+    user_id int,
+    post_id int,
+    content text,
     foreign key (user_id) references dbo.users(id),
     foreign key (post_id) references dbo.posts(id)
-);
-
+)
 create table dbo.likes
 (
-    id int identity(1,1) not null,
-    user_id int not null,
-    post_id int not null,
-    primary key (id),
+    id int primary key,
+    user_id int,
+    post_id int,
     foreign key (user_id) references dbo.users(id),
     foreign key (post_id) references dbo.posts(id)
-);
+)
 
 -- insert 100 random data into all existing tables in the database (users, posts, comments and likes) using loops
--- Language: sql
-declare @i int = 1
-declare @j int = 1
-declare @k int = 1
-declare @l int = 1
-
-while @i <= 100
+declare @i int = 0
+while @i < 100
 begin
     insert into dbo.users
-        (name)
     values
-        ('user' + cast(@i as varchar(50)))
+        (@i, 'user' + cast(@i as varchar))
+    insert into dbo.posts
+    values
+        (@i, @i, 'post' + cast(@i as varchar), 'content' + cast(@i as varchar))
+    insert into dbo.comments
+    values
+        (@i, @i, @i, 'comment' + cast(@i as varchar))
+    insert into dbo.likes
+    values
+        (@i, @i, @i)
     set @i = @i + 1
 end
 
-while @j <= 100
-begin
-    insert into dbo.posts
-        (title, content, user_id)
-    values
-        ('title' + cast(@j as varchar(50)), 'content' + cast(@j as varchar(50)), @j)
-    set @j = @j + 1
-end
-
-while @k <= 100
-begin
-    insert into dbo.comments
-        (content, user_id, post_id)
-    values
-        ('comment' + cast(@k as varchar(50)), @k, @k)
-    set @k = @k + 1
-end
-
-while @l <= 100
-begin
-    insert into dbo.likes
-        (user_id, post_id)
-    values
-        (@l, @l)
-    set @l = @l + 1
-end;
-
-
-
--- show all posts ordered by likes count
--- Language: sql
-select p.id, p.title, p.content, count(l.id) as likes_count
+-- show all posts order by likes count
+select p.id, p.title, count(l.id) as likes_count
 from dbo.posts p
     left join dbo.likes l on p.id = l.post_id
-group by p.id, p.title, p.content
+group by p.id, p.title
 order by likes_count desc
 
 -- show all posts order by likes count using ctes
--- Language: sql
-with
-    cte
-    as
-    (
-        select p.id, p.title, p.content, count(l.id) as likes_count
-        from dbo.posts p
-            left join dbo.likes l on p.id = l.post_id
-        group by p.id, p.title, p.content
-    )
+-- language=SQL
+with cte as
+(
+    select p.id, p.title, count(l.id) as likes_count
+    from dbo.posts p
+        left join dbo.likes l on p.id = l.post_id
+    group by p.id, p.title
+)
 select *
 from cte
 order by likes_count desc
-
-select p.id, p.title, p.content, count(l.id) as likes_count
-from dbo.posts p
-    left join dbo.likes l on p.id = l.post_id
-group by p.id, p.title, p.content
-order by likes_count desc
-
-
